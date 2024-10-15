63cbfdd8217d5696c1a32ed413f56cc673c974ec
softkisik228 <softkisik228@yandex.ru>
2024-10-15 18:20:49 +0300

    rename tables in db

--- autorisation-server/models/models.py
+++ autorisation-server/models/models.py
@@ -3,18 +3,18 @@ from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey, JSO
 
 metadata = MetaData()
 
-users = Table(
-    "users", 
+user = Table(
+    "user", 
     metadata,
     Column("id", Integer, primary_key=True),
     Column("username", String, nullable=False),
     Column("email", String, nullable=False),
     Column("password", String, nullable=False),
-    Column("numbers_id", Integer, ForeignKey("numbers.id"))
+    Column("numbers_id", Integer, ForeignKey("number.id"))
 )
 
-numbers = Table(
-    "numbers", 
+number = Table(
+    "number", 
     metadata,
     Column("id", Integer, primary_key=True),
     Column("numbers", JSON)
