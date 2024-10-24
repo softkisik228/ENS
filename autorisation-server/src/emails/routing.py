5e7d91d6109e7c3e47ae8083893b4d84f6060d80
softkisik228 <softkisik228@yandex.ru>
2024-10-25 00:29:56 +0300

    added celery and sms sender

--- autorisation-server/src/emails/routing.py
+++ autorisation-server/src/emails/routing.py
@@ -5,6 +5,7 @@ from sqlalchemy import select, insert, update, delete
 from database import get_async_session
 from .models.models import email, message
 from .schemas import EmailAdd, MessageAdd, MessageToSend
+from .sms_sender import send_email_notification
 
 router = APIRouter(
     prefix="/emails",
@@ -32,72 +33,58 @@ async def get_messages(message_id: int, session: AsyncSession = Depends(get_asyn
         raise HTTPException(status_code=404, detail="Messages not found")
     return [{"id": mes.id,"messages": mes.messages, "emails": mes.emails} for mes in mes]
 
-
-# @router.post("/add_emails")
-# async def add_emails(new_email: EmailAdd, session: AsyncSession = Depends(get_async_session)):
-#     query = select(email).where(email.c.id == new_email.model_dump()["id"])
-#     result = await session.execute(query)
-#     em = result.fetchone()
-#     if em == None:
-#         stmnt = insert(email).values(new_email)
-#         await session.execute(stmnt)
-#         await session.commit()
-#     else:
-#         combined = {**em.emails, **new_email.model_dump()['emails']}
-#         stmt = (
-#         update(email)
-#         .where(email.c.id == new_email.model_dump()["id"])
-#         .values(emails=combined)
-#         )
-#         await session.execute(stmt)
-#         await session.commit()
-#     return new_email
-
-
 @router.post("/add_emails")
 async def add_emails(new_emails: EmailAdd, session: AsyncSession = Depends(get_async_session)):
-    query = select(email).where(email.c.id == new_emails.model_dump()["id"])
-    result = await session.execute(query)
-    em = result.fetchone()
-    if em == None:
-        stmnt = insert(email).values(new_emails.model_dump())
-        await session.execute(stmnt)
-        await session.commit()
-    else:
-        combined = {**em.emails, **new_emails.model_dump()['numbers']}
-        stmt = (
-        update(email)
-        .where(email.c.id == new_emails.model_dump()["id"])
-        .values(numbers=combined)
-        )
-        await session.execute(stmt)
-        await session.commit()
+    try:
+        query = select(email).where(email.c.id == new_emails.model_dump()["id"])
+        result = await session.execute(query)
+        em = result.fetchone()
+        if em == None:
+            stmnt = insert(email).values(new_emails.model_dump())
+            await session.execute(stmnt)
+            await session.commit()
+        else:
+            combined = {**em.emails, **new_emails.model_dump()['numbers']}
+            stmt = (
+            update(email)
+            .where(email.c.id == new_emails.model_dump()["id"])
+            .values(numbers=combined)
+            )
+            await session.execute(stmt)
+            await session.commit()
+    except Exception as e:
+        print(e)
+        return e
     return new_emails
 
 
 
 @router.post("/add_message")
 async def add_message(new_message: MessageAdd, session: AsyncSession = Depends(get_async_session)):
-    query = select(message).where(message.c.id == new_message.model_dump()["id"])
-    result = await session.execute(query)
-    mes = result.fetchone()
-    if mes == None:
-        stmnt = insert(message).values(new_message.model_dump())
-        await session.execute(stmnt)
-        await session.commit()
-    elif mes.messages == new_message.model_dump()["messages"]:
-        combined = {**mes.emails, **new_message.model_dump()["emails"]}
-        stmt = (
-        update(message)
-        .where(message.c.id == new_message.model_dump()["id"])
-        .values(emails=combined)
-        )
-        await session.execute(stmt)
-        await session.commit()
-    else:
-        stmnt = insert(message).values(new_message.model_dump())
-        await session.execute(stmnt)
-        await session.commit()
+    try:
+        query = select(message).where(message.c.id == new_message.model_dump()["id"])
+        result = await session.execute(query)
+        mes = result.fetchone()
+        if mes == None:
+            stmnt = insert(message).values(new_message.model_dump())
+            await session.execute(stmnt)
+            await session.commit()
+        elif mes.messages == new_message.model_dump()["messages"]:
+            combined = {**mes.emails, **new_message.model_dump()["emails"]}
+            stmt = (
+            update(message)
+            .where(message.c.id == new_message.model_dump()["id"])
+            .values(emails=combined)
+            )
+            await session.execute(stmt)
+            await session.commit()
+        else:
+            stmnt = insert(message).values(new_message.model_dump())
+            await session.execute(stmnt)
+            await session.commit()
+    except Exception as e:
+        print(e)
+        return e
     return new_message
 
 @router.post("/del_message")
@@ -109,6 +96,13 @@ async def delite_message(id: int, message_to_del: str, session: AsyncSession = D
 
 @router.post("/send_message")
 async def send_message(message_to_send: MessageToSend, session: AsyncSession = Depends(get_async_session)):
-    print(message_to_send.emails)
+    try:
+        message_to_send = message_to_send.model_dump()
+        for email in message_to_send["emails"].values():
+            print(email)
+            send_email_notification.delay(message_to_send["message"], email)
+    except Exception as e:
+        print(e)
+        return e
     return {"status": "ok"}
 
--- autorisation-server/src/emails/sms_sender.py
+++ autorisation-server/src/emails/sms_sender.py
@@ -1,3 +1,27 @@
+import smtplib
+from email.message import EmailMessage
+
 from celery import Celery
 
+from config import SMTP_USER, SMTP_PASS
+
+
+SMTP_HOST = "smtp.gmail.com"
+SMTP_PORT = 465
+
 celery = Celery('tasks', broker='redis://localhost:6379')
+
+def get_email_template(message: str, recipient: str):
+    email = EmailMessage()
+    email['Subject'] = 'ENS Sistem'
+    email['From'] = SMTP_USER
+    email['To'] = recipient
+    email.set_content(message)
+    return email
+
+@celery.task
+def send_email_notification(message: str, recipient: str):
+    email = get_email_template(message, recipient)
+    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
+        server.login(SMTP_USER, SMTP_PASS)
+        server.send_message(email)
